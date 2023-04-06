537896f32069ab7c0ef42e02fe317d43917137cf
acs-fernando <acassio.fernando89@gmail.com>
2023-04-06 13:32:50 -0300

    Initial commit

---
+++ lib/screens/transferencia/formulario.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+
+import '../../components/editor.dart';
+import '../../models/transferencia.dart';
+
+const _tituloAppBar = 'Criando Transferência';
+
+const _rotuloCampoValor = 'Valor';
+const _dicaCampoValor = '0.00';
+
+const _rotuloCampoNumeroConta = 'Número da conta';
+const _dicaCampoNumeroConta = '0000';
+
+const _textoBotaoConfirmar = 'Confirmar';
+
+class FormularioTransferencia extends StatefulWidget {
+  const FormularioTransferencia({Key? key}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() {
+    return FormularioTransferenciaState();
+  }
+}
+
+class FormularioTransferenciaState extends State<FormularioTransferencia> {
+  static final TextEditingController _controladorCampoNumeroConta =
+      TextEditingController();
+  static final TextEditingController _controladorCampoValor =
+      TextEditingController();
+
+  void _criaTransferencia(BuildContext context) {
+    final int? numeroConta = int.tryParse(_controladorCampoNumeroConta.text);
+    final double? valor = double.tryParse(_controladorCampoValor.text);
+    if (numeroConta != null && valor != null) {
+      final transferenciaCriada = Transferencia(valor, numeroConta);
+      Navigator.pop(context, transferenciaCriada);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text(_tituloAppBar),
+        ),
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              Editor(
+                  controlador: _controladorCampoNumeroConta,
+                  rotulo: _rotuloCampoNumeroConta,
+                  dica: _dicaCampoNumeroConta),
+              Editor(
+                rotulo: _rotuloCampoValor,
+                controlador: _controladorCampoValor,
+                dica: _dicaCampoValor,
+                icone: Icons.monetization_on,
+              ),
+              ElevatedButton(
+                onPressed: () => _criaTransferencia(context),
+                child: const Text(_textoBotaoConfirmar),
+              ),
+            ],
+          ),
+        ));
+  }
+}
